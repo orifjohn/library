78bf3ce31f69969d7726ebdb5603663114e2abcd
orifjohn <orifjondev@gmail.com>
2022-03-23 09:53:10 +0500

    added viewsets

--- api/views.py
+++ api/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from api.models import Book, LibUser, RentBook
+from .serializers import BookSerializer, LibUserSerializer, RentBookSerializer
 
-# Create your views here.
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book
+    serializer_class = BookSerializer
+
+
+class LibuserViewSet(viewsets.ModelViewSet):
+    queryset = LibUser
+    serializer_class = LibUserSerializer
+
+
+class RentBookViewSet(viewsets.ModelViewSet):
+    queryset = RentBook
+    serializer_class = RentBookSerializer
+
+
